88ba4672c19432804618752aa71f8c1002b47f65
DESKTOP-PC5OSED\Michal <michal.zalobny@gmail.com>
2023-03-19 01:12:19 -0700

    Update user register handlers and userSchemas

--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -1,7 +1,19 @@
 import { FastifyInstance } from "fastify";
 
 import { registerUserHandler } from "./user.controller";
+import { $ref } from "./user.schema";
 
 export const userRoutes = async (server: FastifyInstance) => {
-  server.post("/", registerUserHandler);
+  server.post(
+    "/",
+    {
+      schema: {
+        body: $ref("createUserSchema"),
+        response: {
+          201: $ref("createUserResponseSchema"),
+        },
+      },
+    },
+    registerUserHandler
+  );
 };
